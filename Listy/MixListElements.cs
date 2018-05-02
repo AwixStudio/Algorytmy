80616695fb747e500b888bf74486a18b720aeae9
AwixStudio <awixstudio@gmail.com>
2018-05-02 18:15:47 +0200

    Mieszanie elementów listy.

---
+++ Listy/MixListElements.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+public static class MixListElements {
+
+	public static void MixElements<T>(this List<T> list)  
+	{  
+		System.Random rnd = new System.Random();  
+   		int n = list.Count;
+   		while (n > 1) {  
+        n--;  
+        int k = rnd.Next(n + 1);  
+        T value = list[k];  
+        list[k] = list[n];  
+        list[n] = value;  
+    }  
+	}
+}
